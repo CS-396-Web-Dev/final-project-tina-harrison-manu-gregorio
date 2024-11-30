7f6cf8ed1e5a45c5405cfbfcbc526ad97a9e1cd1
Tina Chen <tinachen2025@u.northwestern.edu>
2024-11-30 06:57:18 -0600

    added placeholder for image

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -7,9 +7,10 @@ const jersey20 = Jersey_20({
 
 export default function Home() {
   return (
-    <div className="text-center mt-3">
+    <div className="text-center mt-3 h-screen">
       <h1 className={`${jersey20.className} text-4xl`}>Tamagotchi</h1>
       <div className="h-px w-full bg-black mt-3"></div>
+      <div className="h-5/6 w-1/4 bg-sky-400 mt-8 ml-28 rounded-md"></div>
     </div>
   );
 }
