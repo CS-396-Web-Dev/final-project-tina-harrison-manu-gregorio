8ce4a043a8f5b749309d24515b234c6705c938a4
Tina Chen <tinachen2025@u.northwestern.edu>
2024-11-30 08:07:19 -0600

    added log text

--- my-app/src/app/Log.tsx
+++ my-app/src/app/Log.tsx
@@ -1,5 +1,9 @@
+import { jersey20 } from "./page";
+
 export default function Log() {
     return (
-        <div className="bg-black h-2/5 mt-9 rounded-md"></div>
+        <div className="bg-black h-2/5 mt-9 rounded-md text-left">
+            <h2 className={`${jersey20.className} text-2xl text-white ml-3 mt-3`}>Log</h2>
+            </div>
     );
 }
