8fb178cb52daa4fcb10525fecafac3a5a260a975
Manu-Deva <manudeva2025@u.northwestern.edu>
2024-12-01 17:46:28 -0600

    mockup-styling
    * changed font color to black when editing pet name
    
    * added border to stats section and gray background to log section
    
    * added progress bars for stats

--- my-app/src/app/LogSection.tsx
+++ my-app/src/app/LogSection.tsx
@@ -1,14 +1,18 @@
 import { usePetContext } from "./PetContext";
 
 export default function LogSection() {
-    const { logs } = usePetContext();
+  const { logs } = usePetContext();
 
-    return (
-        <section className="bg-black rounded-md text-left text-white mt-5 lg:mt-10 p-3">
-            <h2 className="text-2xl">Log</h2>
-            <div className="mt-2">
-                { logs.slice(-10).map((log, index) => <p className="text-xl" key={index}>{ log }</p>) }
-            </div>
-        </section>
-    );
+  return (
+    <section className="bg-gray-900 rounded-md text-left text-white mt-5 lg:mt-10 p-3">
+      <h2 className="text-2xl">Log</h2>
+      <div className="mt-2">
+        {logs.slice(-10).map((log, index) => (
+          <p className="text-xl" key={index}>
+            {log}
+          </p>
+        ))}
+      </div>
+    </section>
+  );
 }
