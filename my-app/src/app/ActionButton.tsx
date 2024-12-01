2df3a2b72e073c2a9524321967f4b70a7bfe59b7
Tina Chen <tinachen2025@u.northwestern.edu>
2024-11-30 20:44:50 -0600

    added action handling

--- my-app/src/app/ActionButton.tsx
+++ my-app/src/app/ActionButton.tsx
@@ -1,14 +1,32 @@
 import { jersey20 } from "./fonts/fonts";
+import { usePetContext } from "./PetContext";
 
 interface ActionButtonProps {
     label: string;
     color: string;
 }
 
+const actionToStatMap: { [key: string]: string } = {
+    "Feed": "Hunger",
+    "Play": "Happiness",
+    "Sleep": "Sleep",
+    "Clean": "Hygiene"
+};
+
 export default function ActionButton({ label, color }: ActionButtonProps) {
+    const { setStats } = usePetContext();
+
+    const handleClick = () => {
+        setStats((prevStats) => {
+            const updatedStats = { ...prevStats };
+            updatedStats[actionToStatMap[label]] += 10;
+            return updatedStats;
+        })
+    }
+
     return (
-        <div className="flex items-center justify-center w-32 h-28 bg-gray-300 rounded-md">
+        <button className="flex items-center justify-center w-32 h-28 bg-gray-300 rounded-md" onClick={handleClick}>
             <h2 className={`${jersey20.className} ${color} text-4xl`}>{ label }</h2>
-        </div>
+        </button>
     );
 }
