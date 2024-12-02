8ea29c0f199fa139947821f6f7754118077f2fab
Tina Chen <tinachen2025@u.northwestern.edu>
2024-12-02 00:33:08 -0600

    made the cat bigger

--- my-app/src/app/ScreenSection.tsx
+++ my-app/src/app/ScreenSection.tsx
@@ -19,8 +19,8 @@ export default function ScreenSection({ petDescriptor }: ScreenProps) {
       <div className="scene sky w-full h-full">
         <div className="cloud absolute bg-white rounded-full h-12 w-24 top-10 left-40 animate-clouds"></div>
         <div className="cloud absolute bg-white rounded-full h-12 w-48 top-20 left-40 animate-clouds"></div>
-        <div className="tamagotchi absolute bottom-12 left-1/2 transform -translate-x-1/2">
-          <img src={petImageSource} alt="Tamagotchi" className="w-12 h-12" />
+        <div className="tamagotchi absolute bottom-6 left-1/2 transform -translate-x-1/2">
+          <img src={petImageSource} alt="Tamagotchi" className="w-32 h-32" />
         </div>
       </div>
     </div>
