0a648a25d7eb0a9968f301d9ead7d6f9246bd872
Tina Chen <tinachen2025@u.northwestern.edu>
2024-12-01 21:45:36 -0600

    created sign out button

---
+++ my-app/src/app/SignOutButton.tsx
@@ -0,0 +1,17 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebaseConfig";
+
+export default function SignOutButton() {
+
+    const handleSignOut = async () => {
+        try {
+            await signOut(auth);
+        } catch (error) {
+            console.error('Could not sign out.');
+        }
+    };
+
+    return (
+        <button className="absolute right-4 bg-gray-300 rounded-md p-2 text-lg" onClick={handleSignOut}>Sign Out</button>
+    );
+}
