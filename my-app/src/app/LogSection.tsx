dca95b42a8b84472dc3a438f2dd4467138f60e0d
Tina Chen <tinachen2025@u.northwestern.edu>
2024-12-01 20:40:35 -0600

    changed spacing between sections

--- my-app/src/app/LogSection.tsx
+++ my-app/src/app/LogSection.tsx
@@ -13,7 +13,7 @@ export default function LogSection() {
 
    
   return (
-    <section className="bg-gray-900 rounded-md text-left text-white mt-5 lg:mt-10 p-3">
+    <section className="bg-gray-900 rounded-md text-left text-white mt-5 lg:mt-8 p-3">
       <h2 className="text-2xl">Log</h2>
       <div ref={logsContainerRef} className="mt-2 max-h-60 overflow-y-auto">
         { logs.map((log, index) => <p className="text-xl" key={index}>{ log }</p>) }
