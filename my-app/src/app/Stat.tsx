70c66ba77d154924e95710224fa29a02827c7992
Tina Chen <tinachen2025@u.northwestern.edu>
2024-12-01 20:49:07 -0600

    make font size conditional

--- my-app/src/app/Stat.tsx
+++ my-app/src/app/Stat.tsx
@@ -6,7 +6,7 @@ interface StatProps {
 export default function Stat({ label, value }: StatProps) {
   return (
     <div className="flex items-center justify-between space-x-4">
-      <label className="font-bold w-1/3 text-right text-xl">{label}:</label>
+      <label className="font-bold w-1/3 text-right sm:text-xl">{label}:</label>
       <div className="w-2/3 h-4 bg-gray-200 rounded-full overflow-hidden">
         <div
           className="h-full bg-green-500"
