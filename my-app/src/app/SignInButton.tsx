966fe6e3ad8a8edbe596448c76c80665f09777d0
Tina Chen <tinachen2025@u.northwestern.edu>
2024-12-01 21:43:12 -0600

    integrated sign in button w/ firebase

--- my-app/src/app/SignInButton.tsx
+++ my-app/src/app/SignInButton.tsx
@@ -1,5 +1,16 @@
+import { signInWithPopup } from "firebase/auth";
+import { auth, googleProvider } from "../../firebaseConfig";
+
 export default function SignInButton() {
+    const handleSignIn = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider);
+        } catch (error) {
+            console.error('Could not sign in.');
+        }
+    };
+
     return (
-        <button className="absolute right-4 bg-gray-300 rounded-md p-2 text-lg">Sign In</button>
+        <button className="absolute right-4 bg-gray-300 rounded-md p-2 text-lg" onClick={handleSignIn}>Sign In</button>
     );
 }
