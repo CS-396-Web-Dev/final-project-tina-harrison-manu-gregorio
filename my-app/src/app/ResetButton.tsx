01d4a94576ab0ad56eded277ac915e5135f05cbb
Tina Chen <tinachen2025@u.northwestern.edu>
2024-12-02 00:38:36 -0600

    added missing alt

--- my-app/src/app/ResetButton.tsx
+++ my-app/src/app/ResetButton.tsx
@@ -4,6 +4,6 @@ export default function ResetButton() {
     const { resetPet } = usePetContext();
 
     return (
-        <img className="absolute left-4 w-5 h-5 cursor-pointer" src="/reset.png" onClick={resetPet} />
+        <img className="absolute left-4 w-5 h-5 cursor-pointer" src="/reset.png" onClick={resetPet} alt="Reset pet" />
     );
 }
