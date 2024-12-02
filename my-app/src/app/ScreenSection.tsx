122d99b4f7bbb9ec1742262647ad666c444ebea1
Tina Chen <tinachen2025@u.northwestern.edu>
2024-12-02 00:29:01 -0600

    minor styling changes

--- my-app/src/app/ScreenSection.tsx
+++ my-app/src/app/ScreenSection.tsx
@@ -1,8 +1,8 @@
-interface screenProps {
+interface ScreenProps {
   petDescriptor: string;
 }
 
-export default function ScreenSection({ petDescriptor }: screenProps) {
+export default function ScreenSection({ petDescriptor }: ScreenProps) {
   let petImageSource = "/normal-cat.png";
 
   if (
@@ -15,7 +15,7 @@ export default function ScreenSection({ petDescriptor }: screenProps) {
 
   return (
     <div className="lg:w-1/3 h-5/6 mt-5 bg-sky-400 rounded-md relative">
-      <div className="ground w-full h-16 bg-green-500 absolute bottom-0"></div>
+      <div className="ground w-full h-16 bg-green-500 absolute bottom-0 rounded-md"></div>
       <div className="scene sky w-full h-full">
         <div className="cloud absolute bg-white rounded-full h-12 w-24 top-10 left-40 animate-clouds"></div>
         <div className="cloud absolute bg-white rounded-full h-12 w-48 top-20 left-40 animate-clouds"></div>
